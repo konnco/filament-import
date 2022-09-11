f623648191e82ff92d332c1af47048e631b64aec
Franky So <frankyso.mail@gmail.com>
2022-09-11 10:56:06 +0700

    Cleanup

--- resources/views/import.blade.php
+++
@@ -1,3 +0,0 @@
-<div>
-    @livewire('filament-import::uploader')
-</div>
---
+++ tests/TestCase.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Konnco\FilamentImport\Tests;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Orchestra\Testbench\TestCase as Orchestra;
+use Konnco\FilamentImport\FilamentImportServiceProvider;
+
+class TestCase extends Orchestra
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            FilamentImportServiceProvider::class,
+        ];
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+        config()->set('database.default', 'testing');
+
+        /*
+        $migration = include __DIR__.'/../database/migrations/create_skeleton_table.php.stub';
+        $migration->up();
+        */
+    }
+}
