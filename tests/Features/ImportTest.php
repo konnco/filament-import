cf7a72a5c98883c24db24066217763d5e7fb13d7
Franky So <frankyso.mail@gmail.com>
2022-09-23 10:03:37 +0700

    added first

---
+++ tests/Features/ImportTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Collection;
+// use Spatie\ModelInfo\Attributes\AttributeFinder;
+// use Spatie\ModelInfo\Tests\TestSupport\Models\ExtendedTypesModel;
+// use Spatie\ModelInfo\Tests\TestSupport\Models\TestModel;
+
+it('can import csv, xlsx files', function () {
+    expect(true)->toBeTrue();
+});
+
+it('can publishing config', function () {
+    expect(true)->toBeTrue();
+});
+
+it('can make field required', function () {
+    expect(true)->toBeTrue();
+});
+
+it('can disable mass create', function () {
+    expect(true)->toBeTrue();
+});
+
+it('can manipulate single field', function () {
+    expect(true)->toBeTrue();
+});
+
+it('can manipulate mass field', function () {
+    expect(true)->toBeTrue();
+});
+
+it('can make grid column', function () {
+    expect(true)->toBeTrue();
+});
+
+it('can render filament field', function () {
+    expect(true)->toBeTrue();
+});
+
+it('can save json casting field', function () {
+    expect(true)->toBeTrue();
+});
+
+it('can validating field', function () {
+    expect(true)->toBeTrue();
+});
