89b5eb7f2eb477d2881a4509382cd78b5cad76de
Kenneth Sese <ksese@hope4tomorrow.net>
2023-02-07 20:53:50 -0600

    Fixes column matching on first field

--- src/Actions/ImportAction.php
+++ src/Actions/ImportAction.php
@@ -172,7 +172,7 @@ class ImportAction extends Action
                 }
 
                 $selected = array_search($field->getName(), $options);
-                if ($selected != false) {
+                if ($selected !== false) {
                     $set($field->getName(), $selected);
                 }
 
