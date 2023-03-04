97f5d64337ab195279d52fb917ef87530ab7edaa
Kenneth Sese <ksese@hope4tomorrow.net>
2023-03-03 20:50:53 -0600

    Adds translations

--- resources/lang/en/actions.php
+++ resources/lang/en/actions.php
@@ -3,6 +3,8 @@
 return [
     'import' => 'Import',
     'skip_header' => 'Skip header',
+    'skip_footer' => 'Skip footer',
+    'skip_footer_count' => 'Number of footer rows to skip',
     'match_to_column' => 'Match data to column',
     'import_failed' => 'Import failed, please check your import file and try again',
     'import_failed_title' => 'Import Failed',
