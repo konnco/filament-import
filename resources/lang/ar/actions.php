307d1907d51ca9547a0261ced8efd7bfb3020732
Saif Allah Khaled <Saifallak@yahoo.com>
2023-05-29 21:39:51 +0300

    Create actions.php

---
+++ resources/lang/ar/actions.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'import' => 'استيراد',
+    'skip_header' => 'تخطي العنوان',
+    'match_to_column' => 'تطابق البيانات مع العمود',
+    'import_failed' => 'فشل الاستيراد ، يرجى التحقق من ملف الاستيراد والمحاولة مرة أخرى',
+    'import_failed_title' => 'فشل الاستيراد',
+    'import_succeeded' => 'استيراد:count الصفوف الناجحة ،:skipped تم تخطي صف (الصفوف)',
+    'import_succeeded_title' => 'نجح الاستيراد',
+];
