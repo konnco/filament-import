bd97a29b3ce7932d3f602f8c2b62b798265c6289
Adrià Ardila <contacto@adriaardila.com>
2023-08-26 20:02:26 +0200

    Remove not needed parameter

--- src/Actions/ImportAction.php
+++ src/Actions/ImportAction.php
@@ -164,7 +164,7 @@ class ImportAction extends Action
                 $options = $this->cachedHeadingOptions;
 
                 if (count($options) === 0) {
-                    $options = $this->toCollection($filePath, $this->getTemporaryDisk())->first()?->first()->filter(fn ($value) => $value != null)->map('trim')->toArray();
+                    $options = $this->toCollection($filePath)->first()?->first()->filter(fn ($value) => $value != null)->map('trim')->toArray();
                 }
 
                 $selected = array_search($field->getName(), $options);
