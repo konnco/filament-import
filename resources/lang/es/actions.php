3d2f967c0976553912e51688bacfe344077d774d
Anibal Moradel <amoradel@outlook.com>
2023-12-14 09:22:43 -0600

    feat: :sparkles: Update languages es. en, pt_br

--- resources/lang/es/actions.php
+++ resources/lang/es/actions.php
@@ -6,8 +6,8 @@ return [
     'match_to_column' => 'Asignar los campos a sus columnas',
     'import_failed' => 'La importación ha fallada. Revise el archivo y pruebe de nuevo.',
     'import_failed_title' => 'Importación Fallida',
-    'import_skipped' => 'Importación de :count fila(s), :skipped fila(s) fueron omitida(s).',
-    'import_skipped_title' => 'Importación con filas omitidas',
+    'import_info' => 'Importación de :count fila(s), :skipped fila(s) fueron omitida(s).',
+    'import_info_title' => 'Importación con filas omitidas',
     'import_succeeded' => 'Importación exitosa de :count fila(s).',
     'import_succeeded_title' => 'Importación Exitosa',
 ];
